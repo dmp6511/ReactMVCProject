e30a5d2dd66fb93b1dbd95bc3b18714b97ec4180
De'Andre Perry <dmp6511@rit.edu>
2024-04-18 17:00:27 -0400

    fixes

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -1,63 +1,60 @@
 // model for the song blog
 const mongoose = require('mongoose');
-const _ = require('underscore');
-
 
 let blogModel = {};
 
 // blog schema
 // accepts title, artist, genre and a rating. Users can also add comments to the blog
 const blogSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    artist: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    genre: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    rating: {
-        type: Number,
-        max: 5,
-        required: true,
-    },
-    
-    // description (optional)
-    description: {
-        type: String,
-        required: false,
-        trim: true,
-    },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  artist: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  rating: {
+    type: Number,
+    max: 5,
+    required: true,
+  },
 
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  // description (optional)
+  description: {
+    type: String,
+    required: false,
+    trim: true,
+  },
 
-    owner: {
-        type: mongoose.Schema.ObjectId,
-        required: true,
-        ref: 'Account',
-    },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Account',
+  },
 });
 
 // converts a doc to something we can store in redis later on
 blogSchema.statics.toAPI = (doc) => ({
-    title: doc.title,
-    artist: doc.artist,
-    genre: doc.genre,
-    rating: doc.rating,
-    description: doc.description,
-    createdAt: doc.createdAt,
+  title: doc.title,
+  artist: doc.artist,
+  genre: doc.genre,
+  rating: doc.rating,
+  description: doc.description,
+  createdAt: doc.createdAt,
 });
 
 blogModel = mongoose.model('Blog', blogSchema);
 module.exports = blogModel;
-
--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = (app) => {
-app.get('/getBlogs', mid.requiresLogin, controllers.Blog.getBlogs);
+  app.get('/getBlogs', mid.requiresLogin, controllers.Blog.getBlogs);
 
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
